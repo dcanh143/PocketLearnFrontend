0798010f47f854e3ab817508d916dc817fb58aae
dcanh143 <72723381+dcanh143@users.noreply.github.com>
2022-12-04 23:10:28 +0000

    Deploying to gh-pages from @ dcanh143/PocketLearnFrontend@91859cabf3a6f818d578224afab27605d2b60870 🚀

---
+++ assets/base.service.052f6e96.js
@@ -0,0 +1,1 @@
+import"./index.8a2bd1dd.js";const e=()=>({"Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("jwt_token")});export{e as a};
--- assets/base.service.38f4ea39.js
+++
@@ -1,1 +0,0 @@
-import"./index.4390b9ab.js";const e=()=>({"Content-Type":"application/json",Authorization:"Bearer "+localStorage.getItem("jwt_token")});export{e as a};
