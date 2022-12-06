ed804d103af04d981f5f2594716f23596c3064a9
NguyenDucAnh <ducanhkhtn@gmail.com>
2022-12-06 12:30:05 +0700

    update

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ if (
 }
 
 /* Default title tag */
-const defaultDocumentTitle = "Admin One Vue 3 Tailwind";
+const defaultDocumentTitle = "Pocket Learn";
 
 /* Set document title from route meta */
 router.afterEach((to) => {
