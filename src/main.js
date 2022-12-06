23f467acb0f3e42d6e75c62429aad18304bdc670
NguyenDucAnh <ducanhkhtn@gmail.com>
2022-12-06 12:31:10 +0700

    update

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import router from "./router";
 import { useMainStore } from "@/stores/main.js";
 import { useStyleStore } from "@/stores/style.js";
 import { darkModeKey, styleKey } from "@/config.js";
-import Notification from "@kyvg/vue3-notification";
+import Notifications from "@kyvg/vue3-notification";
 
 import "./css/main.css";
 
@@ -14,7 +14,7 @@ import "./css/main.css";
 const pinia = createPinia();
 
 /* Create Vue app */
-createApp(App).use(router).use(pinia).use(Notification).mount("#app");
+createApp(App).use(router).use(pinia).use(Notifications).mount("#app");
 
 /* Init Pinia stores */
 const mainStore = useMainStore(pinia);
